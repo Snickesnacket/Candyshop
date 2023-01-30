9c1ce9b83e96770496e466a0ef23fc275eed294f
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-01-30 21:03:41 +0100

    changes URL in postorder

--- frontend/src/post.ts
+++ frontend/src/post.ts
@@ -3,7 +3,7 @@ import { IConfirmationResult, IOrder } from "./interfaces";
 
 // ** POST THE ORDER TO API**
 export const postOrder = async (newOrder: IOrder) => {
-  const res = await fetch('https://www.bortakvall.se/api/orders', {
+  const res = await fetch('http://localhost:3000/orders', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
